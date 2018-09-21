906d45ff6f2eb1cbd67df7cf8cd42b8781fb7af3
yr <yannkristen.roth@students.bfh.ch>
2018-09-21 11:16:39 +0200

    DivideAndConquerMemo interface: start

--- src/week1/DivideAndConquerableMemo.java
+++ src/week1/DivideAndConquerableMemo.java
@@ -1,55 +1,27 @@
 package week1;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
 
-public interface DivideAndConquerableMemo<OutputType> {
+public interface DivideAndConquerableMemo<OutputType> extends DivideAndConquerable<OutputType> {
 
-    // 4 basic methods to split the recursion into feasible smaller problems
-    /** fibonacci example:
-     * 1. do we have a base case? 'isBasic()'?
-     * 2. if yes, return its value 'baseFunction()'
-     * 3. if no, reduce case complexity, i.e. decompose()
-     * 4. & recombine(): f(5) = f(4) + f(3)
-     */
-    boolean isBasic();
-    OutputType baseFunction();
-    List<? extends DivideAndConquerableMemo<OutputType>> decompose();
-    OutputType recombine(List<OutputType> intermediateResults);
-
-    // what is this????
-    default List<? extends DivideAndConquerableMemo<OutputType>> stump() {
-        return new ArrayList<DivideAndConquerableMemo<OutputType>>(0);
-    }
+    // NEW: add a class cache variable
+    HashMap<Integer, Integer> fibonacciHashMap = new HashMap<>();
 
 
-    // DEFAULT divideAndConquer method which returns a type of 'OutputType'
-    // example: fibonacci will return an integer (i.e. 'int')
+    /** default method to be overriden*/
+    @Override
     default OutputType divideAndConquer() {
-        // if 'this' is a basic case, return the 'basic result'
-        // example: fibonacci(1)=1, f(0)=0
         if (this.isBasic()) {
             return this.baseFunction();
         }
 
-        // if this is NOT a base case, do what needs to be done to reduce the problem size
-        // example: f(5) = f(4) + f(3) which is what will need to be returned!
-        // if the input is f(n), the output must be the list of [f(n-1), f(n-2)]
         List<? extends DivideAndConquerableMemo<OutputType>> subcomponents = this.decompose();
 
-        /** make a list of intermediate results
-         * splitting f(5) into f(4) and f(3) makes a list of the results (i.e. f(4) and f(3))
-         * that can be looked up later.
-         * -> avoid recalculating already calculated stuff!
-         * This is JUST a result list of the intermediate results! */
         List<OutputType> intermediateResults = new ArrayList<OutputType>(subcomponents.size());
-
-        // now add each subcomponent to the intermediateResults list
-        // here 'add' is NOT a sum!
-        // It ADDS the subcomponents to the list!
         subcomponents.forEach(subcomponent -> intermediateResults.add(subcomponent.divideAndConquer()));
 
-        // since it's not a base case, we need to return the recombination, i.e. f(4)+f(3), here IT IS the sum!
         return recombine(intermediateResults);
     }
 }
