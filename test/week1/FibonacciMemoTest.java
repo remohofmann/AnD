95354fba64ff600c5d11ede3c58cad653715cc73
yr <yannkristen.roth@students.bfh.ch>
2018-09-22 22:48:52 +0200

    DivideAndConquerMemo: map solution minor test output styling

--- test/week1/FibonacciMemoTest.java
+++ test/week1/FibonacciMemoTest.java
@@ -11,101 +11,30 @@ import java.util.Map;
 
 public class FibonacciMemoTest {
 
-    private FibonacciMemo<Integer> fiboSimple0 = new FibonacciMemo<>(0);
-    private FibonacciMemo<Integer> fiboSimple1 = new FibonacciMemo<Integer>(1);
-    private FibonacciMemo<Integer> fiboSimple2 = new FibonacciMemo<Integer>(2);
-    private FibonacciMemo<Integer> fiboSimple3 = new FibonacciMemo<Integer>(3);
-    private FibonacciMemo<Integer> fiboSimple5 = new FibonacciMemo<Integer>(5);
-    private FibonacciMemo<Integer> fiboSimple10 = new FibonacciMemo<Integer>(10);
-
-
     @Test
-    public void calculateFibonacciNumbers() {
+    public void FibonacciMemoComparedToFibonacciSimpleTest() {
+        int s = 10;
         int l = 35;
-        System.out.println("Fibonacci 10,11,12,...,35");
+        System.out.println("Fibonacci " + s + ",...," + l);
         System.out.println("Fibonacci Simple");
         long simpleStart = System.currentTimeMillis();
-        for (int i = 10; i < l; i++) {
+        for (int i = s; i < l; i++) {
             System.out.print("f(" + i + ")=" + new FibonacciSimple<Integer>(i).divideAndConquer().intValue());
             System.out.print(" ");
         }
         long simpleEnd = System.currentTimeMillis();
         System.out.println();
-        System.out.println("Time [milli seconds]: " + (simpleEnd - simpleStart));
+        System.out.println("Time: " + (simpleEnd - simpleStart) + " ms");
         System.out.println();
         System.out.println("Fibonacci with Memo");
         simpleStart = System.currentTimeMillis();
-        for (int i = 10; i < l; i++) {
+        for (int i = s; i < l; i++) {
             HashMap fibValMap = new HashMap<>();
             System.out.print("f(" + i + ")=" + new FibonacciMemo<Integer>(i).divideAndConquer(fibValMap).intValue());
             System.out.print(" ");
         }
         simpleEnd = System.currentTimeMillis();
         System.out.println();
-        System.out.println("Time [milli seconds]: " + (simpleEnd - simpleStart));
-
-        // Measurements (1. number of Fibonacci numbers // 2. time)
-        HashMap simpleMap = new HashMap();
-        HashMap memoMap = new HashMap();
-
-        // specify maximum number of fibonacci numbers
-        // Simple
-        /*for (int j = 10; j <= 35; j = j + 5) {
-
-            System.out.println();
-            System.out.println("Simple");
-            int[] tmpSimpleArray = new int[j];
-
-            simpleStart = System.nanoTime();
-            for (int i = 0; i < j; i++) {
-                FibonacciSimple tempF = new FibonacciSimple(i);
-                tmpSimpleArray[i] = (int) tempF.divideAndConquer();
-            }
-            simpleEnd = System.nanoTime();
-
-            for (int i = 0; i < j; i++) {
-                System.out.print(i + "=" + tmpSimpleArray[i]);
-                System.out.print(" ");
-                System.out.print(" ");
-            }
-            // Add measurements to simpleMap
-            simpleMap.put(j, simpleEnd - simpleStart);
-        }
-
-
-        // Memo
-        for (int j = 10; j <= 35; j = j + 5) {
-            System.out.println("\nMemo");
-            HashMap reusableFibonacciNumbers = new HashMap();
-            int[] tmpMemoArray = new int[j];
-
-            long memoStart = System.nanoTime();
-            for (int i = 0; i < j; i++) {
-                FibonacciMemo tempF = new FibonacciMemo(i);
-                tmpMemoArray[i] = (int) tempF.divideAndConquer(reusableFibonacciNumbers);
-            }
-            long memoEnd = System.nanoTime();
-
-            for (int i = 0; i < j; i++) {
-                System.out.print(i + "=" + tmpMemoArray[i]);
-                System.out.print(" ");
-                System.out.print(" ");
-            }
-            // Add measurements to memoMap
-            memoMap.put(j, memoEnd - memoStart);
-        }
-        // Print out measurements
-        // Simple
-        System.out.println();
-        System.out.println();
-        System.out.println("SimpleMap times");
-        System.out.println(simpleMap.toString());
-        System.out.println();
-
-        // Memo
-        System.out.println("Memo times");
-        System.out.println(memoMap.toString());*/
-
-
+        System.out.println("Time: " + (simpleEnd - simpleStart) +" ms");
     }
 }
