c0f5c4296f952cc0cc175ae5c321938faf55a9b4
hrkun <lukas.zoller@gmx.ch>
2018-09-23 21:15:24 +0200

    Memo-Daten in Chart, Logarithmische Achse nicht möglich

--- src/week1/ViewDataController.java
+++ src/week1/ViewDataController.java
@@ -35,22 +35,34 @@ public class ViewDataController extends Application {
         TreeMap<Integer, Integer> memoTreeMap = new TreeMap<Integer, Integer>(this.memoMap);
 
         stage.setTitle("Fibonacci Algorithms");
+
         final NumberAxis xAxis = new NumberAxis();
         final NumberAxis yAxis = new NumberAxis();
+
         final LineChart<Number, Number> lineChart = new LineChart<Number, Number>(xAxis,yAxis);
         lineChart.setTitle("Fibonacci Data");
         xAxis.setLabel("Size of Fibonacci Line");
         yAxis.setLabel("Computation Time");
 
 
-        XYChart.Series series1 = new XYChart.Series();
-        Set<Integer> keys = simpleTreeMap.keySet();
-        for(Integer key: keys){
-            series1.getData().add(new XYChart.Data(key, this.simpleMap.get(key)));
+        XYChart.Series simpleSeries = new XYChart.Series();
+        Set<Integer> simpleKeys = simpleTreeMap.keySet();
+        for(Integer key: simpleKeys){
+            simpleSeries.getData().add(new XYChart.Data(key, this.simpleMap.get(key)));
         }
 
+        XYChart.Series memoSeries = new XYChart.Series();
+        Set<Integer> memoKeys = memoTreeMap.keySet();
+        for(Integer key: memoKeys){
+            memoSeries.getData().add(new XYChart.Data(key, this.memoMap.get(key)));
+        }
+
+        simpleSeries.setName("Simple");
+        memoSeries.setName("Memoization");
+
         Scene scene = new Scene(lineChart, 800, 600);
-        lineChart.getData().add(series1);
+        lineChart.getData().add(simpleSeries);
+        lineChart.getData().add(memoSeries);
 
         stage.setScene(scene);
         stage.show();
