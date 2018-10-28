de3a6914646b25088f3f4389559260ce58fb9872
Yann Roth <yannkristen.roth@students.bfh.ch>
2018-10-28 19:15:20 +0100

    logarithm Annäherung per binary search: adds possibility to define base.

--- src/ex_7_binarySearch/Main.java
+++ src/ex_7_binarySearch/Main.java
@@ -25,20 +25,21 @@ public class Main {
         /*************************** logarithm ***************************************/
         double logEpsilon = 0.000001;
         arr = new int[]{2, 4, 8, 16, 32, 64};
+        double base = 2;
         System.out.println("Epsilon: " + logEpsilon);
         System.out.println();
         System.out.println("---- LOGARITHM----");
         System.out.println("----Iterative Function----");
         Logarithm tmp;
         for (int j = 0; j < arr.length; j++) {
-            tmp = new Logarithm(arr[j], logEpsilon);
-            System.out.println("logarithm-base-2(" + arr[j] + ") = " + tmp.logarithmIterative());
+            tmp = new Logarithm(arr[j], base, logEpsilon);
+            System.out.println("logarithm-base-("+base+")(" + arr[j] + ") = " + tmp.logarithmIterative());
         }
         System.out.println();
         System.out.println("---- Recursive Function----");
         for (int j = 0; j < arr.length; j++) {
-            tmp = new Logarithm(arr[j], logEpsilon);
-            System.out.println("Logarithm-base-2(" + arr[j] + ") = " + tmp.logarithmRecursive());
+            tmp = new Logarithm(arr[j], base, logEpsilon);
+            System.out.println("Logarithm-base-("+ base +")(" + arr[j] + ") = " + tmp.logarithmRecursive());
         }
 
 
