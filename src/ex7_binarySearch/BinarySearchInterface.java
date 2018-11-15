8f9900f9658ce31f524f8d5e243a563250c393a4
yr <yannkristen.roth@students.bfh.ch>
2018-11-15 11:03:53 +0100

    ex7 binary search: binary search interface minor refactoring.

--- src/ex7_binarySearch/BinarySearchInterface.java
+++ src/ex7_binarySearch/BinarySearchInterface.java
@@ -13,10 +13,13 @@ public interface BinarySearchInterface<E> {
         int mid = (left + right) / 2;
         int comparison = comparator.compare(target, a.get(mid));
         if (comparison == 0) return mid;
-        else if (mid == left || mid == right )
+        else if (mid == left || mid == right) {
             comparison = -1;
-        else if (comparison < 0 ) return binarySearch(a, left, mid - 1, target, comparator);
-        else return binarySearch(a, mid + 1, right, target, comparator);
+        } else if (comparison < 0) {
+            return binarySearch(a, left, mid - 1, target, comparator);
+        } else {
+            return binarySearch(a, mid + 1, right, target, comparator);
+        }
         return comparison;
     }
 }
